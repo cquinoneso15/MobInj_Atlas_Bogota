a5fe55ead9a0870928051d9d5471b353d6d6cc5a
cquinoneso15 <cmquinonesoeding@hotmail.com>
2025-03-11 19:05:07 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -80,7 +80,11 @@ async function mergeGeoJSONandCSV2(geojsonPath, csvPath,csvPath2) {
 function convertNumericPropertiesToFloat(properties) {
     const floatProperties = {};
     for (const key in properties) {
-        floatProperties[key] = isNumeric(properties[key]) ? parseFloat(properties[key]) : properties[key];
+        if (isNumeric(properties[key])) {
+            floatProperties[key] = parseFloat(properties[key].replace(",", "."));
+        } else {
+            floatProperties[key] = properties[key];
+        }
     }
     return floatProperties;
 }
